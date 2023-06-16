8c13453f210c7b7256464f00e3cc330491cbd1b8
Anna Rafaela Bispo de Santana <anna.santana@compasso.com.br>
2023-06-16 10:11:50 -0300

    create record ParkingSpotDto

---
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
@@ -0,0 +1,23 @@
+package com.api.parkingcontrol.dtos;
+
+import jakarta.validation.constraints.*;
+
+public record ParkingSpotDto(
+        @NotBlank
+        String parkingSpotNumber,
+        @NotBlank
+        @Size(max = 7)
+        String licensePlateCar,
+        @NotBlank String brandCar,
+        @NotBlank
+        String modelCar,
+        @NotBlank
+        String colorCar,
+        @NotBlank
+        String responsibleName,
+        @NotBlank
+        String apartment,
+        @NotBlank
+        String block
+) {
+}
